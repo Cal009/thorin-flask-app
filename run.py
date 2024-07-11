16aec844bf2e8975d8d1d3675e7dbdc6d56aaddd
Callum Priestman <callum.priestman@hotmail.co.uk>
2024-07-11 17:46:07 +0000

    static template created

--- run.py
+++ run.py
@@ -26,8 +26,7 @@ def careers():
 
 
 if __name__ == "__main__":
-    app.run9(
+    app.run(
         host=os.environ.get("IP", "0.0.0.0"),
-        port=int(os.enviro.get("PORT", "5000")),
+        port=int(os.environ.get("PORT", "5000")),
         debug=True)
-
