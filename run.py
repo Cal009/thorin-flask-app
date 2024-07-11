c8f8b897415a8fa899f5ca90cc9f848c489e00fb
Cal009 <153200653+Cal009@users.noreply.github.com>
2024-07-11 02:53:52 +0100

    Links for pages in place

--- run.py
+++ run.py
@@ -9,6 +9,11 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 if __name__ == "__main__":
     app.run9(
         host=os.environ.get("IP", "0.0.0.0"),
